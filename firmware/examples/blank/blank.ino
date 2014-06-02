d886b5f6be5becb11448fbd39b7ce9c5a14ad47a
Paul Kourany <pkourany@users.noreply.github.com>
2014-06-02 08:42:20 -0400

    Update to tested and working code

--- firmware/examples/blank/blank.ino
+++ firmware/examples/blank/blank.ino
@@ -3,22 +3,19 @@
 // Fingerprint sensor connected to RX/TX (Serial1)
 // on the Spark
 
-void setup() {
-  // initialize both serial ports:
+void setup()  
+{
+  // Open serial communications and wait for port to open:
   Serial.begin(57600);
   Serial1.begin(57600);
+
 }
 
-void loop() {
-  // read from Serial1 (Fingerprint reader), send to Serial (USB to PC)
-  if (Serial1.available()) {
-    int inByte = Serial1.read();
-    Serial.write(inByte);
-  }
+void loop() // run over and over
+{
+  while (Serial1.available())
+    Serial.write(Serial1.read());
 
-  // read from Serial (USB to PC), send to Serial1 (Fingerprint reader)
-  if (Serial.available()) {
-    int inByte = Serial.read();
-    Serial1.write(inByte);
-  }
+  while (Serial.available())
+    Serial1.write(Serial.read());
 }
